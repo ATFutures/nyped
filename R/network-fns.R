64c645bca0accc9f8475e3ce7cf8ac6dbd909203
mpadge <mark.padgham@email.com>
2019-09-09 15:09:44 +0200

    :heavy_plus_sign:ped_osm_id fn

--- R/network-fns.R
+++ R/network-fns.R
@@ -1,21 +1,19 @@
-
-#' cut_network_to_pts
+#' ped_osm_id
 #'
-#' Cut the New York City street network into portions surrounding each
-#' pedestrian counting point.
-#'
+#' Get OSM IDs nearest to pedestrian count points
 #' @param data_dir The directory in which data are to be, or have previously
 #' been, downloaded.
-#' @param k Exponential decay parameter to be used in spatial interaction
-#' models. This deterines the radius at which to cut the network.
+#' @return A `data.frame` of pedestrian counts, associated spatial coordinates,
+#' and OSM IDs of nearest points on network
 #' @export
-cut_network_to_pts <- function (data_dir, k = 700)
+ped_osm_id <- function (data_dir)
 {
     p <- nyped_data ()
     xy <- data.frame (sf::st_coordinates (p$geometry))
 
     f <- file.path (data_dir, "osm", "ny-hw.Rds")
     hw <- readRDS (f)
+    requireNamespace ("dodgr")
     dodgr::dodgr_cache_off ()
     message (cli::symbol$pointer, " Weighting street network",
              appendLF = FALSE)
@@ -24,21 +22,48 @@ cut_network_to_pts <- function (data_dir, k = 700)
     xy$id <- v$id [dodgr::match_points_to_graph (v, xy)]
     message ("\r", cli::symbol$tick, " Weighted street network ")
 
+    return (xy)
+}
+
+
+#' cut_network_to_pts
+#'
+#' Cut the New York City street network into portions surrounding each
+#' pedestrian counting point.
+#'
+#' @param k Exponential decay parameter to be used in spatial interaction
+#' models. This deterines the radius at which to cut the network.
+#' @param p Pedestrian counts including OSM IDs of nearest points, as returned
+#' from \link{ped_osm_id}.
+#' @param data_dir The directory in which data are to be, or have previously
+#' been, downloaded - only needed if `p` is not provided.
+#' @export
+cut_network_to_pts <- function (k = 700, p = NULL, data_dir)
+{
+    if (is.null (p))
+        p <- ped_osm_id (data_dir)
+
     message (cli::symbol$pointer, " Cutting into ", nrow (p), " components")
     dlim <- k * 12 # equivalent to point at which exp(-d/k) <= 1e-12
-    pb <- utils::txtProgressBar (style = 3)
-    for (i in seq (nrow (xy)))
-    {
-        id <- xy$id [i]
-        res <- dodgr::dodgr_isoverts (net, from = id, dlim = dlim)
+
+    # set up parallel job over origin points
+    no_cores <- parallel::detectCores () - 1
+    cl <- parallel::makeCluster (no_cores)
+
+    st0 <- Sys.time ()
+    chk <- parallel::parLapply (cl, xy$id, function (i) {
+        res <- dodgr::dodgr_isoverts (net, from = i, dlim = dlim)
         index <- which (net$.vx0 %in% res$id | net$.vx1 %in% res$id)
         net_cut <- net [index, ]
         nm <- file.path (data_dir, "osm", "ny-cut",
-                         paste0 ("ny-hw", id, ".Rds"))
+                         paste0 ("ny-hw", i, ".Rds"))
         saveRDS (net_cut, nm)
-        utils::setTxtProgressBar (pb, i / nrow (xy))
-    }
-    close (pb)
+             })
+
+    parallel::stopCluster (cl)
+
+    st <- as.numeric (difftime (Sys.time (), st0, units = "secs"))
     message (cli::symbol$tick, " Successfully cut network into ",
-             nrow (p), " components")
+             nrow (p), " components in ",
+             formatC (st, format = "f", digits = 1), "s")
 }
